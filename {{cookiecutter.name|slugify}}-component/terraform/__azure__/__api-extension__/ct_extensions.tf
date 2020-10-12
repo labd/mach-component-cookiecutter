df84cbf009bed0152fab1495505da109e6b187ca
Tim Leguijt <t.leguijt@labdigital.nl>
2020-10-12 16:21:42 +0200

    Move function keys deployment to ct_extensions

--- {{cookiecutter.name|slugify}}-component/terraform/__azure__/__api-extension__/ct_extensions.tf
+++ {{cookiecutter.name|slugify}}-component/terraform/__azure__/__api-extension__/ct_extensions.tf
@@ -3,6 +3,40 @@ resource "commercetools_api_client" "{{ cookiecutter.component_identifier }}" {
   scope = local.ct_scopes
 }
 
+# Get the functions keys out of the app
+resource "azurerm_template_deployment" "function_keys" {
+  name = "${azurerm_function_app.{{ cookiecutter.component_identifier }}.name}-function-keys"
+  parameters = {
+    "functionApp" = azurerm_function_app.{{ cookiecutter.component_identifier }}.name
+  }
+  resource_group_name = var.resource_group_name
+  deployment_mode     = "Incremental"
+
+  template_body = <<BODY
+  {
+      "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
+      "contentVersion": "1.0.0.0",
+      "parameters": {
+          "functionApp": {"type": "string", "defaultValue": ""}
+      },
+      "variables": {
+          "functionAppId": "[resourceId('Microsoft.Web/sites', parameters('functionApp'))]"
+      },
+      "resources": [
+      ],
+      "outputs": {
+          "functionkey": {
+              "type": "string",
+              "value": "[listkeys(concat(variables('functionAppId'), '/host/default'), '2018-11-01').functionKeys.default]"                                                                                }
+      }
+  }
+  BODY
+}
+
+locals {
+  function_app_key = lookup(azurerm_template_deployment.function_keys.outputs, "functionkey")
+}
+
 resource "commercetools_api_extension" "order_actions" {
   key = "create-order"
 

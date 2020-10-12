92bfde0392b2f87a51ba1750d574597e6e7128d8
Tim Leguijt <t.leguijt@labdigital.nl>
2020-10-12 14:18:45 +0200

    Remove unneeded argument

--- {{cookiecutter.name|slugify}}-component/__azure__/azure-upload.sh
+++ {{cookiecutter.name|slugify}}-component/__azure__/azure-upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 STORAGE_ACCOUNT_KEY=`az storage account keys list -g {{ cookiecutter.shared_resource_group }} -n {{ cookiecutter.function_storage_account }} --query [0].value -o tsv`
-az storage blob upload --account-name {{ cookiecutter.function_storage_account }} --account-key ${STORAGE_ACCOUNT_KEY} -c {{ cookiecutter.function_container_name }} -f $1 -c {{ cookiecutter.function_container_name }} -n $1
+az storage blob upload --account-name {{ cookiecutter.function_storage_account }} --account-key ${STORAGE_ACCOUNT_KEY} -c {{ cookiecutter.function_container_name }} -f $1 -n $1

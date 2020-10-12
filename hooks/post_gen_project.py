20cc6f24b16d662cca4c6c51d67ff8f6984aecb1
Tim Leguijt <t.leguijt@labdigital.nl>
2020-10-12 16:21:59 +0200

    Add template for public api component

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -18,23 +18,40 @@ def use_resources(directory: str):
     if not os.path.isdir(directory):
         raise ValueError(f"{directory} is not a directory")
 
+    print(f"use resources from {directory}")
+
     target_dir = os.path.dirname(directory)
-
-    print("-----------------------------")
-    print(f"{directory} to {target_dir}")
-
 
     for file_name in os.listdir(directory):
         if file_name.startswith("."):
             continue
 
         source = os.path.join(directory, file_name)
-        # if os.path.isfile(source):
         shutil.move(source, target_dir)
-        # else:
-        #     shutil.copytree(source, target_dir)
 
     delete_resource(directory)
+
+
+def unpack(directory_pattern: str):
+    """Unpacks a directory pattern to several directories"""
+    unpacked = [
+        directory_pattern
+    ]
+
+    if "*" in directory_pattern:
+        parent_dir = os.path.dirname(directory_pattern)
+
+        # Very simple matching on start and end string, no regex here
+        start, end = os.path.basename(directory_pattern).split("*")
+
+        subdirs = os.listdir(parent_dir)
+        unpacked = [
+            os.path.join(parent_dir, dir_) for dir_ in os.listdir(parent_dir)
+            if dir_.startswith(start) and dir_.endswith(end)
+        ]
+
+    return unpacked
+
 
 with open(MANIFEST) as manifest_file:
     directories = [
@@ -45,9 +62,11 @@ with open(MANIFEST) as manifest_file:
 
 for directory in directories:
     if directory.startswith("!"):
-        delete_resource(directory[1:])
+        for dir_ in unpack(directory[1:]):
+            delete_resource(os.path.join(PROJECT_DIRECTORY, dir_))
     else:
-        use_resources(os.path.join(PROJECT_DIRECTORY, directory))
+        for dir_ in unpack(directory):
+            use_resources(os.path.join(PROJECT_DIRECTORY, dir_))
 
 print("cleanup complete, removing manifest...")
 delete_resource(MANIFEST)
---
+++ {{cookiecutter.name|slugify}}-component/terraform/__azure__/__public_api__/application_insights.tf
@@ -0,0 +1,103 @@
+# errors, duration triggers, dead letter queues?
+resource "azurerm_application_insights" "insights" {
+  name                 = lower(format("%s-appi-%s", var.name_prefix, var.short_name))
+  location             = var.resource_group_location
+  resource_group_name  = var.resource_group_name
+  application_type     = "web"
+  daily_data_cap_in_gb = 1
+  retention_in_days    = 90
+
+  tags = var.tags
+}
+
+resource "azurerm_monitor_metric_alert" "exceptions" {
+  name                = format("%s-exceptions", var.short_name)
+  resource_group_name = var.resource_group_name
+  scopes              = [azurerm_application_insights.insights.id]
+  description         = "Action will be triggered when uncaught exceptions are present"
+
+  frequency   = "PT5M"
+  window_size = "PT5M"
+  severity    = 2
+
+  criteria {
+    metric_namespace = "microsoft.insights/components"
+    metric_name      = "exceptions/count"
+    aggregation      = "Count"
+    operator         = "GreaterThanOrEqual"
+    threshold        = 0.9
+  }
+
+  action {
+    action_group_id = var.monitor_action_group_id
+
+    # data sent with the webhook
+    webhook_properties = {
+      "component" : var.short_name
+    }
+  }
+
+  tags = var.tags
+
+  # this custom metric is only created after the function app is created...
+  depends_on = [azurerm_function_app.{{ cookiecutter.component_identifier }}]
+}
+
+
+resource "azurerm_application_insights_web_test" "ping" {
+  name                    = lower(format("%s-appi-%s-ping", var.name_prefix, var.short_name))
+  location                = var.resource_group_location
+  resource_group_name     = var.resource_group_name
+  application_insights_id = azurerm_application_insights.insights.id
+  kind                    = "ping"
+  frequency               = 300
+  timeout                 = 60
+  enabled                 = true
+  geo_locations = [
+    "emea-nl-ams-azr",
+    "emea-se-sto-edge",
+    "emea-ru-msa-edge",
+  ]
+
+  configuration = <<XML
+<WebTest Name="PingTest" Enabled="True" Timeout="0" Proxy="default" StopOnError="False" RecordedResultFile="">
+  <Items>
+    <Request Method="GET" Version="1.1" Url="https://${azurerm_function_app.{{ cookiecutter.component_identifier }}.name}.azurewebsites.net/{{ cookiecutter.function_name }}/healthchecks?code=${var.short_name}" ThinkTime="0" Timeout="300" ParseDependentRequests="True" FollowRedirects="True" RecordResult="True" Cache="False" ResponseTimeGoal="0" Encoding="utf-8" ExpectedHttpStatusCode="200" ExpectedResponseUrl="" ReportingName="" IgnoreHttpStatusCode="False" />
+  </Items>
+</WebTest>
+XML
+}
+
+
+resource "azurerm_monitor_metric_alert" "ping" {
+  name                = format("%s-ping-response", var.short_name)
+  resource_group_name = var.resource_group_name
+  scopes              = [azurerm_application_insights.insights.id]
+  description         = "Action will be triggered when ping response is too long"
+
+  frequency   = "PT5M"
+  window_size = "PT5M"
+  severity    = 3
+
+  criteria {
+    metric_namespace = "microsoft.insights/components"
+    metric_name      = "availabilityresults/duration"
+    aggregation      = "Maximum"
+    operator         = "GreaterThan"
+    threshold        = 1000
+  }
+
+  action {
+    action_group_id = var.monitor_action_group_id
+
+    # data sent with the webhook
+    webhook_properties = {
+      "component" : var.short_name
+    }
+  }
+
+  tags = var.tags
+
+  # this custom metric is only created after the function app is created...
+  depends_on = [azurerm_function_app.{{ cookiecutter.component_identifier }}]
+}

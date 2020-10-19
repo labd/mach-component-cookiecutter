4c375fae7e97f0e2437dce4c79aa5a2891dbe346
Tim Leguijt <t.leguijt@labdigital.nl>
2020-10-19 15:30:13 +0200

    Add availability checks for all function types

--- {{cookiecutter.name|slugify}}-component/terraform/__azure__/__default__/application_insights.tf
+++ {{cookiecutter.name|slugify}}-component/terraform/__azure__/__default__/application_insights.tf
@@ -46,3 +46,65 @@ resource "azurerm_monitor_metric_alert" "exceptions" {
   # this custom metric is only created after the function app is created...
   depends_on = [azurerm_function_app.{{ cookiecutter.component_identifier }}]
 }
+
+
+resource "azurerm_application_insights_web_test" "ping" {
+  name                    = lower(format("%s-appi-%s-ping", var.name_prefix, var.short_name))
+  location                = var.resource_group_location
+  resource_group_name     = var.resource_group_name
+  application_insights_id = azurerm_application_insights.insights.id
+  kind                    = "ping"
+  frequency               = 300
+  timeout                 = 60
+  enabled                 = true
+  geo_locations = [
+    "emea-nl-ams-azr",
+    "emea-se-sto-edge",
+    "emea-ru-msa-edge",
+  ]
+
+  configuration = <<XML
+<WebTest Name="PingTest" Enabled="True" Timeout="0" Proxy="default" StopOnError="False" RecordedResultFile="">
+  <Items>
+    <Request Method="GET" Version="1.1" Url="https://${azurerm_function_app.{{ cookiecutter.component_identifier }}.name}.azurewebsites.net/{{ cookiecutter.function_name }}/healthchecks?code=${var.short_name}" ThinkTime="0" Timeout="300" ParseDependentRequests="True" FollowRedirects="True" RecordResult="True" Cache="False" ResponseTimeGoal="0" Encoding="utf-8" ExpectedHttpStatusCode="200" ExpectedResponseUrl="" ReportingName="" IgnoreHttpStatusCode="False" />
+  </Items>
+</WebTest>
+XML
+}
+
+
+resource "azurerm_monitor_metric_alert" "ping" {
+  name                = format("%s-ping-response", var.short_name)
+  resource_group_name = var.resource_group_name
+  scopes              = [azurerm_application_insights.insights.id]
+  description         = "Action will be triggered when ping response is too long"
+
+  frequency   = "PT5M"
+  window_size = "PT5M"
+  severity    = 3
+
+  criteria {
+    metric_namespace = "microsoft.insights/components"
+    metric_name      = "availabilityresults/duration"
+    aggregation      = "Maximum"
+    operator         = "GreaterThan"
+    threshold        = 1000
+  }
+
+      for_each = var.monitor_action_group_id == "" ? [] : [1]
+      content {
+    action {
+      action_group_id = var.monitor_action_group_id
+
+      # data sent with the webhook
+      webhook_properties = {
+        "component" : var.short_name
+      }
+    }
+  }
+
+  tags = var.tags
+
+  # this custom metric is only created after the function app is created...
+  depends_on = [azurerm_function_app.{{ cookiecutter.component_identifier }}]
+}

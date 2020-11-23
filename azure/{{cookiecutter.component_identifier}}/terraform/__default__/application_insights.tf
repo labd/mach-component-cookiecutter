6f24fa40cb8a19bff7cabdde8223161ac498471f
Tim Leguijt <t.leguijt@labdigital.nl>
2020-11-23 09:37:54 +0100

    Implement correct infra for CT subscription

--- azure/{{cookiecutter.component_identifier}}/terraform/__default__/application_insights.tf
+++ azure/{{cookiecutter.component_identifier}}/terraform/__default__/application_insights.tf
@@ -47,7 +47,7 @@ resource "azurerm_monitor_metric_alert" "exceptions" {
   depends_on = [azurerm_function_app.main]
 }
 
-
+{% if cookiecutter.use_commercetools_api_extension|int or cookiecutter.use_public_api|int -%}
 resource "azurerm_application_insights_web_test" "ping" {
   name                    = lower(format("%s-appi-%s-ping", var.name_prefix, var.short_name))
   location                = var.resource_group_location
@@ -110,3 +110,80 @@ resource "azurerm_monitor_metric_alert" "ping" {
   # this custom metric is only created after the function app is created...
   depends_on = [azurerm_function_app.main]
 }
+{% endif %}
+
+{% if cookiecutter.use_commercetools_subscription|int -%}
+esource "azurerm_monitor_metric_alert" "topic_order_signals_dlq" {
+  name                = format("%s-topic-order-signals-dlq", var.short_name)
+  resource_group_name = var.resource_group_name
+  scopes              = [data.azurerm_eventgrid_topic.ct_signals.id]
+  description         = "Action will be triggered when topic messages are deadlettered."
+
+  frequency   = "PT5M"
+  window_size = "PT5M"
+  severity    = 2
+
+  criteria {
+    metric_namespace = "Microsoft.EventGrid/topics"
+    metric_name      = "DeadLetteredCount"
+    aggregation      = "Total"
+    operator         = "GreaterThanOrEqual"
+    threshold        = 0.9
+    dimension {
+      name     = "DeadLetterReason"
+      operator = "Include"
+      values   = ["MaxDeliveryAttemptsExceeded"]
+    }
+  }
+
+  dynamic "action" {
+    for_each = var.monitor_action_group_id == "" ? [] : [1]
+
+    content {
+      action_group_id = var.monitor_action_group_id
+
+      # data sent with the webhook
+      webhook_properties = {
+        "component" : var.short_name
+      }
+    }
+  }
+
+  tags = var.tags
+}
+
+# Double since the previous alert doesn't seem to work reliably, hopefully this will always work
+resource "azurerm_monitor_metric_alert" "dlq_files_exist" {
+  name                = format("%s-sa-dlq-files-exist", var.short_name)
+  resource_group_name = var.resource_group_name
+  scopes              = [format("%s/blobServices/default", azurerm_storage_account.dlq.id)]
+  description         = "Action will be triggered when DLQ files exist."
+
+  frequency   = "PT1M"
+  window_size = "PT1H"
+  severity    = 2
+
+  criteria {
+    metric_namespace = "Microsoft.Storage/storageAccounts/blobServices"
+    metric_name      = "BlobCount"
+    aggregation      = "Average"
+    operator         = "GreaterThan"
+    threshold        = 0.0
+  }
+
+  dynamic "action" {
+    for_each = var.monitor_action_group_id == "" ? [] : [1]
+
+    content {
+      action_group_id = var.monitor_action_group_id
+
+      # data sent with the webhook
+      webhook_properties = {
+        "component" : var.short_name
+      }
+    }
+  }
+
+  tags = var.tags
+}
+{% endif %}
--- azure/{{cookiecutter.component_identifier}}/terraform/__default__/storage_account.tf
+++ azure/{{cookiecutter.component_identifier}}/terraform/__default__/storage_account.tf
@@ -8,3 +8,23 @@ resource "azurerm_storage_account" "main" {
   
   tags = var.tags
 }
+
+{% if cookiecutter.use_commercetools_subscription|int -%}
+resource "azurerm_storage_account" "dlq" {
+  name                     = replace(lower(format("%s-sa-%s-dlq", var.name_prefix, var.short_name)), "-", "")
+  location                 = var.resource_group_location
+  resource_group_name      = var.resource_group_name
+  account_tier             = "Standard"
+  account_replication_type = local.storage_type
+  allow_blob_public_access = false
+
+  tags = var.tags
+}
+
+
+resource "azurerm_storage_container" "container_dlq" {
+  name                  = "dlq"
+  storage_account_name  = azurerm_storage_account.dlq.name
+  container_access_type = "private"
+}
+{% endif %}

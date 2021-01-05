6f003f4a57a0964c315a73cd7dc96d09e01ba876
Tim Leguijt <t.leguijt@labdigital.nl>
2021-01-05 22:14:16 +0100

    Add build and package script

---
+++ aws/{{cookiecutter.component_identifier}}/build.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+VERSION=$(git rev-parse --short HEAD 2>/dev/null || echo "dev" )
+BASENAME={{ cookiecutter.name|slugify }}
+NAME=$BASENAME-$VERSION
+BUILD_NAME=$NAME
+ARTIFACT_NAME="${NAME}.zip"
+
+package () {
+    {% if cookiecutter.language == "node" %}
+    yarn package:prod
+    {% else %}
+    # TODO
+    {% endif %}
+}
+
+upload () {
+    {% if cookiecutter.language == "node" %}
+    src=".serverless/{{ cookiecutter.name|slugify }}.zip"
+    dest=$ARTIFACT_NAME
+    aws s3 cp $src s3://{{ cookiecutter.lambda_s3_repository }}/$dest
+    {% else %}
+    # TODO
+    {% endif %}
+}
+
+version () {
+    echo "Version: '${VERSION}'"
+    echo "Name: '${NAME}'"
+    echo "Artifect name: '${ARTIFACT_NAME}'"
+}
+
+case $1 in
+    package)
+        package $2 $3
+    ;;
+    upload)
+        upload $2
+    ;;
+    version)
+        version
+    ;;
+esac

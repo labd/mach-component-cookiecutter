f0bdf2db10883e21d42e2721bcc5513528007023
Tim Leguijt <t.leguijt@labdigital.nl>
2021-01-06 14:05:00 +0100

    Move senry to Koa app file

--- aws/{{cookiecutter.component_identifier}}/__node__/src/rest/app.ts
+++ aws/{{cookiecutter.component_identifier}}/__node__/src/rest/app.ts
@@ -1,11 +1,36 @@
-import bodyParser from 'koa-bodyparser'
 import Koa from 'koa'
+import bodyParser from 'koa-bodyparser'
+import cors from '@koa/cors';
+import { Sentry } from '../lib/sentry'
+import { addExtensionMethods } from '@sentry/tracing';
+
 import router from './routes'
 import logger from './middleware/logger-middleware'
 
 const app = new Koa()
 app.use(logger())
+app.use(cors())
 app.use(bodyParser())
-app.use(router.middleware())
 
+if (process.env.SENTRY_DSN) {
+  addExtensionMethods()
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    tracesSampleRate: 1.0,
+    enabled: true,
+    release: process.env.RELEASE,
+    environment: process.env.ENVIRONMENT,
+    integrations: [new Sentry.Integrations.Http({ tracing: true })],
+  })
+  app.on('error', (err, ctx) => {
+    Sentry.withScope(function (scope) {
+      scope.addEventProcessor(function (event) {
+        return Sentry.Handlers.parseRequest(event, ctx.request)
+      })
+      Sentry.captureException(err)
+    })
+  })
+}
+
+app.use(router.middleware())
 export default app

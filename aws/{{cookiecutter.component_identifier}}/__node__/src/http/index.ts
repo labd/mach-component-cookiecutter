3719f3f035029349f5dfdc24b9acc5f17434196b
Tim Leguijt <t.leguijt@labdigital.nl>
2021-01-06 23:00:11 +0100

    Rename include_graphql option

--- aws/{{cookiecutter.component_identifier}}/__node__/src/http/index.ts
+++ aws/{{cookiecutter.component_identifier}}/__node__/src/http/index.ts
@@ -1,6 +1,6 @@
 import serverless from 'serverless-http'
 import app from './rest/app'
-{% if cookiecutter.include_graphql|int -%}
+{% if cookiecutter.include_graphql_server|int -%}
 import { server } from './graphql/server';
 
 // Combine GraphQL server with Koa routing

442cacb4baa1986f7b2578d876532ea2135cf05d
Tim Leguijt <t.leguijt@labdigital.nl>
2021-01-14 08:49:57 +0100

    Revert "Avoid packaging up aws-sdk"
    This reverts commit 0f70c585c54aa87426b09ee35d78a0d6c322cb8e.

--- aws/{{cookiecutter.component_identifier}}/__node__/webpack.config.ts
+++ aws/{{cookiecutter.component_identifier}}/__node__/webpack.config.ts
@@ -1,4 +1,5 @@
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import nodeExternals from 'webpack-node-externals';
 import serverlessWebpack from 'serverless-webpack';
 import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
 import { Configuration, DefinePlugin, WebpackPluginInstance } from 'webpack';
@@ -6,7 +7,7 @@ import { Configuration, DefinePlugin, WebpackPluginInstance } from 'webpack';
 const config: Configuration = {
  mode: serverlessWebpack.lib.webpack.isLocal ? 'development' : 'production',
   entry: serverlessWebpack.lib.entries,
-  externals: [{'aws-sdk': 'commonjs aws-sdk'}],
+  externals: [nodeExternals()],
   devtool: serverlessWebpack.lib.webpack.isLocal ? 'inline-cheap-module-source-map' : 'source-map',
   target: 'node',
   node: false,

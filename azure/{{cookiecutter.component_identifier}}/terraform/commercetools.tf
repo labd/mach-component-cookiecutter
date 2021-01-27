7fde62e45999b3b206e398ea334806e9101bbf3c
Tim Leguijt <t.leguijt@labdigital.nl>
2021-01-27 12:10:41 +0100

    Use new data source for function key

--- azure/{{cookiecutter.component_identifier}}/terraform/commercetools.tf
+++ azure/{{cookiecutter.component_identifier}}/terraform/commercetools.tf
@@ -29,38 +29,16 @@ resource "commercetools_subscription" "main" {
 }{% endif %}
 
 {% if cookiecutter.use_commercetools_api_extension|int -%}
-# Get the functions keys out of the app
-resource "azurerm_template_deployment" "function_keys" {
-  name = "${azurerm_function_app.main.name}-function-keys"
-  parameters = {
-    "functionApp" = azurerm_function_app.main.name
-  }
+data "azurerm_function_app_host_keys" "function_keys" {
+  name                = azurerm_function_app.main.name
   resource_group_name = var.resource_group_name
-  deployment_mode     = "Incremental"
-
-  template_body = <<BODY
-  {
-      "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
-      "contentVersion": "1.0.0.0",
-      "parameters": {
-          "functionApp": {"type": "string", "defaultValue": ""}
-      },
-      "variables": {
-          "functionAppId": "[resourceId('Microsoft.Web/sites', parameters('functionApp'))]"
-      },
-      "resources": [
-      ],
-      "outputs": {
-          "functionkey": {
-              "type": "string",
-              "value": "[listkeys(concat(variables('functionAppId'), '/host/default'), '2018-11-01').functionKeys.default]"                                                                                }
-      }
-  }
-  BODY
+  depends_on = [
+    azurerm_function_app.main
+  ]
 }
 
 locals {
-  function_app_key = lookup(azurerm_template_deployment.function_keys.outputs, "functionkey")
+  function_app_key = data.azurerm_function_app_host_keys.function_keys.default_function_key
 }
 
 resource "commercetools_api_extension" "main" {

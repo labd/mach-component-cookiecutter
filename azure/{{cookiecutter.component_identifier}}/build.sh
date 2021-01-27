be7fd383a7c66078c8a63ac7ac87bdd8333c5444
Tim Leguijt <t.leguijt@labdigital.nl>
2021-01-27 16:08:54 +0100

    Fix variable issue

--- azure/{{cookiecutter.component_identifier}}/build.sh
+++ azure/{{cookiecutter.component_identifier}}/build.sh
@@ -20,7 +20,7 @@ package () {
 upload () {
     src="build/${NAME}.zip"
     dest=$ARTIFACT_NAME
-    STORAGE_ACCOUNT_KEY=`az storage account keys list -g {{ cookiecutter.shared_resource_group }} -n { cookiecutter.function_storage_account }} --query [0].value -o tsv`
+    STORAGE_ACCOUNT_KEY=`az storage account keys list -g {{ cookiecutter.shared_resource_group }} -n {{ cookiecutter.function_storage_account }} --query [0].value -o tsv`
     az storage blob upload --account-name {{ cookiecutter.function_storage_account }} --account-key ${STORAGE_ACCOUNT_KEY} -c {{ cookiecutter.function_container_name }} -f ${src} -n ${dest}
 }
 

a12f2de077c65d8e4905770003cada2f034293d0
bramkaashoek <b.kaashoek@labdigital.nl>
2021-02-05 15:58:50 +0100

    replace webpack and update eslint config

---
+++ aws/{{cookiecutter.component_identifier}}/__node__/webpack.config.ts
@@ -0,0 +1,38 @@
+import path from 'path'
+import serverlessWebpack from 'serverless-webpack'
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin'
+import { Configuration, DefinePlugin, WebpackPluginInstance } from 'webpack'
+import nodeExternals from 'webpack-node-externals'
+import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin'
+
+const config: Configuration = {
+  mode: serverlessWebpack.lib.webpack.isLocal ? 'development' : 'production',
+  entry: serverlessWebpack.lib.entries,
+  externals: [nodeExternals()],
+  devtool: serverlessWebpack.lib.webpack.isLocal ? 'inline-cheap-module-source-map' : 'source-map',
+  target: 'node',
+  node: false,
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  optimization: {
+    minimize: false,
+  },
+  resolve: {
+    plugins: [new TsconfigPathsPlugin()],
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin(),
+    new DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+  ].filter(Boolean) as WebpackPluginInstance[],
+}
+module.exports = config

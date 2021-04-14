613f3246c453f3add45699476ed8f8e08e91df48
Tim Leguijt <t.leguijt@labdigital.nl>
2021-04-14 14:24:39 +0200

    Improve build script: deploy git tagged versions

--- aws/{{cookiecutter.component_identifier}}/build.sh
+++ aws/{{cookiecutter.component_identifier}}/build.sh
@@ -1,29 +1,54 @@
 #!/bin/bash
 
 VERSION=$(git rev-parse --short HEAD 2>/dev/null || echo "dev" )
+TAGS=$(git tag)
 BASENAME={{ cookiecutter.name|slugify }}
-NAME=$BASENAME-$VERSION
-BUILD_NAME=$NAME
-ARTIFACT_NAME="${NAME}.zip"
+AWS_BUCKET_NAME="{{ cookiecutter.lambda_s3_repository }}"
+
+artifact () {
+    echo "${BASENAME}-$1.zip"
+}
+
+ARTIFACT_NAME=$(artifact $VERSION)
 
 package () {
     {% if cookiecutter.language == "node" -%}
-    yarn package:prod{% else -%}
-    # TODO{% endif %}
+    yarn package:prod{% elif cookiecutter.language == "python" -%}
+    python3 setup.py sdist bdist_wheel
+    python3 -m pip install dist/*.whl -t ./build
+    cp handler.py ./build
+    clean
+    cd build && zip -9 -r $ARTIFACT_NAME .
+    {% endif %}
 }
 
 upload () {
     {% if cookiecutter.language == "node" -%}
-    src=".serverless/{{ cookiecutter.name|slugify }}.zip"
-    dest=$ARTIFACT_NAME
-    aws s3 cp $src s3://{{ cookiecutter.lambda_s3_repository }}/$dest{% else -%}
-    # TODO{% endif %}
+    src=".serverless/{{ cookiecutter.name|slugify }}.zip"{% elif cookiecutter.language == "python" -%}
+    src="build/${ARTIFACT_NAME}"
+    {% endif %}
+    aws s3 cp $src s3://$AWS_BUCKET_NAME/$ARTIFACT_NAME
+    for TAG in $TAGS
+    do
+        echo "Uploading tagged ${TAG}"
+        aws s3 cp $src s3://$AWS_BUCKET_NAME/$(artifact $TAG)
+    done
 }
 
 version () {
     echo "Version: '${VERSION}'"
-    echo "Name: '${NAME}'"
-    echo "Artifect name: '${ARTIFACT_NAME}'"
+    echo "Artifact name: '${ARTIFACT_NAME}'"
+    for TAG in $TAGS
+    do
+        echo " - $(artifact $TAG)"
+    done
+}
+
+clean () {
+    {% if cookiecutter.language == "python" -%}
+    find . -name '*.pyc' -delete
+    find . -name '__pycache__' -delete
+    find . -name '*.egg-info' | xargs rm -rf{% endif %}
 }
 
 case $1 in
@@ -35,5 +60,8 @@ case $1 in
     ;;
     version)
         version
+    ;;
+    clean)
+        clean
     ;;
 esac
--- azure/{{cookiecutter.component_identifier}}/build.sh
+++ azure/{{cookiecutter.component_identifier}}/build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 VERSION=$(git rev-parse --short HEAD 2>/dev/null || echo "dev" )
+TAGS=$(git tag)
 BASENAME=$(basename $PWD)
 NAME={{ cookiecutter.name|slugify }}-$VERSION
-BUILD_NAME=$NAME
-ARTIFACT_NAME="${NAME}.zip"
+
+artifact () {
+    echo "${BASENAME}-$1.zip"
+}
+
+ARTIFACT_NAME=$(artifact $VERSION)
 
 package () {
     {% if cookiecutter.language == "node" -%}
@@ -12,9 +17,9 @@ package () {
     mkdir -p build
     func pack -o build/$NAME
     # For some reason; func pack doesnt do this for us;
-    find . -name function.json | xargs zip -ur build/$NAME.zip
-    zip -ur build/$NAME.zip dist
-    zip -ur build/$NAME.zip node_modules
+    find . -name function.json | xargs zip -ur build/$ARTIFACT_NAME
+    zip -ur build/$ARTIFACT_NAME dist
+    zip -ur build/$ARTIFACT_NAME node_modules
     {% else -%}
     mkdir -p build
 	func pack --build-native-deps --python
@@ -22,16 +27,23 @@ package () {
 }
 
 upload () {
-    src="build/${NAME}.zip"
-    dest=$ARTIFACT_NAME
+    src="build/${ARTIFACT_NAME}"
     STORAGE_ACCOUNT_KEY=`az storage account keys list -g {{ cookiecutter.shared_resource_group }} -n {{ cookiecutter.function_storage_account }} --query [0].value -o tsv`
-    az storage blob upload --account-name {{ cookiecutter.function_storage_account }} --account-key ${STORAGE_ACCOUNT_KEY} -c {{ cookiecutter.function_container_name }} -f ${src} -n ${dest}
+    az storage blob upload --account-name {{ cookiecutter.function_storage_account }} --account-key ${STORAGE_ACCOUNT_KEY} -c {{ cookiecutter.function_container_name }} -f ${src} -n ${ARTIFACT_NAME}
+    for TAG in $TAGS
+    do
+        echo "Uploading tagged ${TAG}"
+        az storage blob upload --account-name {{ cookiecutter.function_storage_account }} --account-key ${STORAGE_ACCOUNT_KEY} -c {{ cookiecutter.function_container_name }} -f ${src} -n ${TAG}
+    done
 }
 
 version () {
     echo "Version: '${VERSION}'"
-    echo "Name: '${NAME}'"
-    echo "Artifect name: '${ARTIFACT_NAME}'"
+    echo "Artifact name: '${ARTIFACT_NAME}'"
+    for TAG in $TAGS
+    do
+        echo " - $(artifact $TAG)"
+    done
 }
 
 case $1 in
